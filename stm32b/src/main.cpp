5833f0bc28ca5cce4c850ab2c860742982f08161
franzthelofe <jfranzbalagonza+dev@gmail.com>
2026-08-28 04:31:35 +0800

    add .code-workspace

--- stm32b/src/main.cpp
+++ stm32b/src/main.cpp
@@ -1,28 +1,51 @@
 
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 #define LED_PIN PC13
+#define TXD_PIN PB8
+#define RXD_PIN PB9
+#define IN1_PIN PA0
+#define IN2_PIN PA1
+
+SoftwareSerial btSerial(RXD_PIN, TXD_PIN);
 
 void setup()
 {
   pinMode(LED_PIN, OUTPUT);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  btSerial.begin(9600);
   Serial.begin(115200);
-}
 
-unsigned long prevMillis = 0;
-const int interval = 1000;
+}
 
 void loop()
 {
-<<<<<<< HEAD
-  unsigned long curMillis = millis();
-=======
-  unsigned long curMillis = millis(); 
->>>>>>> body
-
-  while (curMillis - prevMillis >= interval)
+  static unsigned long prevMillis = 0;
+  if (millis() - prevMillis >= 1000)
   {
-    prevMillis = curMillis;
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    prevMillis = millis();
+    btSerial.print("Uptime: ");
+    btSerial.println(millis() / 1000);
+  }
+
+  if (btSerial.available())
+  {
+    char c = btSerial.read();
+    
+    if (c == '1')
+    {
+      digitalWrite(LED_PIN, LOW);
+      digitalWrite(IN1_PIN, HIGH);
+      digitalWrite(IN2_PIN, LOW);
+    }
+
+    if (c == '0')
+    {
+      digitalWrite(LED_PIN, HIGH);
+      digitalWrite(IN1_PIN, LOW);
+      digitalWrite(IN2_PIN, HIGH);
+    }
   }
 }
